143c7bde7c92eabfdd3d51d5647610ffaa0f52ec
Phuong Ngo <ngompung@gmail.com>
2019-08-01 18:34:25 +0300

    Change styles and timer

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from 'components/App';
 // eslint-disable-next-line no-unused-vars
 import styles from 'static/styles.css';
+import additional from 'static/additional.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
