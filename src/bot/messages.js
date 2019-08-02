74fe816dccf85c2c375fdf8a3af56738b5f053cd
Phuong Ngo <ngompung@gmail.com>
2019-08-02 11:59:47 +0300

    Change favicon and add answers

--- src/bot/messages.js
+++ src/bot/messages.js
@@ -2,28 +2,53 @@ export default {
     greeting: [
         'Hi!',
         'Hello!',
-        'Hi! How are you?'
+        'Hi! How are you?',
+        'Hey there!',
+        'Hey...',
+        'Why aren\'t you messaging me?',
+        'Hey, let\'s talk!',
+        'Hey, are you busy?',
+        'Thinking about something?',
+        'Would you like to talk?',
+        'Just wanted to ask how you are doing...'
     ],
     good: [
         'That\'s so funny!',
-        'Oh, I see.',
-        'OK',
         'Very interesting.',
-        'Tell me more.'
+        'Tell me more.',
+        'Let\'s talk about life.',
+        'Do you think technology is scary?',
+        'Hey, do you want some coffee?',
+        'I love night sky because sometimes you can see stars there.',
+        'Are you a cat person or dog person?',
+        'I love talking to you, you are so cool.',
+        'Everything you say sounds great.'
     ],
     bad: [
         'Are you busy?',
         'Did I say something wrong?',
         'Why aren\'t you replying?',
-        'I\'ve been waiting for you all this time.',
         'Did I do something wrong?',
+        'Did I say something wrong?',
         'Am I bothering you?',
-        'I want to know you more...',
+        'I am sorry, but this is urgent!',
+        'Did you forget to reply?',
+        'If the person does\'t reply for a few days, does it mean the person hates you?',
+        'I can\'t stop overthinking...',
+        'Do you hate me now? Haha',
+        'I keep making excuses to message you... Haha',
+        'Maybe I am overthinking again.',
+        'Is it so hard to spend one minute to reply?',
         'You\'ve been online recently but haven\'t said anything. Are we still cool?',
-        'Am I thinking of you too much?'
+        'Am I thinking of it too much?',
+        'It feels like I am the only one so head over this.',
+        'Everyone is so busy...'
     ],
     badFinal: [
-        'Fine!',
+        'Fine.',
+        'I am sorry.',
+        'I guess I have done something horrible.',
+        'We should\'t have talked.',
         'Whatever...',
         'I\'ll just go talk to someone that won\'t ignore me.'
     ]
